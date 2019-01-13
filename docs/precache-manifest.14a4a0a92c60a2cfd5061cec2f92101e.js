abed87b66ae659a466cde3a2d7ede0e4f149364a
mave108 <rakeshkumar8900@gmail.com>
2019-01-13 13:09:01 +0530

    production build

---
+++ docs/precache-manifest.14a4a0a92c60a2cfd5061cec2f92101e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab75dcdd384f9ca6c44f",
+    "url": "/xebia_start_war_assessment/static/js/runtime~main.ab75dcdd.js"
+  },
+  {
+    "revision": "66c0bf56fde036ff0f95",
+    "url": "/xebia_start_war_assessment/static/js/main.66c0bf56.chunk.js"
+  },
+  {
+    "revision": "7aaeb2bb85f7eef5fe58",
+    "url": "/xebia_start_war_assessment/static/js/1.7aaeb2bb.chunk.js"
+  },
+  {
+    "revision": "66c0bf56fde036ff0f95",
+    "url": "/xebia_start_war_assessment/static/css/main.af8af7ea.chunk.css"
+  },
+  {
+    "revision": "982b650a4890a30f78a9053ce58fb28f",
+    "url": "/xebia_start_war_assessment/index.html"
+  }
+];
